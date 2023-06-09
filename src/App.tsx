547ec3aa2958c3a3f58b36db8169cc42acf1dc75
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-06-09 13:51:41 +0200

    added tailwind, basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,32 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const config = [
+  { heading: 'Lorem ipsum 1', options: ['Tak', 'Nie'] },
+  { heading: 'Lorem ipsum 2', options: ['Tak', 'Nie'] },
+];
+
+export default function App() {
+  const [slide, setSlide] = useState(0);
+
+  const setOption = () => {
+    setSlide(slide + 1);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col justify-center items-center w-full min-h-screen p-5">
+      <h2 className="text-3xl mb-12 text-center">{config[slide].heading}</h2>
+
+      <div className="flex flex-wrap gap-5">
+        {config[slide].options.map((option, i) => (
+          <button
+            key={i}
+            onClick={setOption}
+            className="px-4 py-2 rounded-md bg-green-500 text-base"
+          >
+            {option}
+          </button>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
-
-export default App;
